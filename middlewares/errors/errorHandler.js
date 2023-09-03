a123f2cbea19ecbea29ced30e07eb7ff5ba9be11
Nikita Norseev <nikita.norseev@gmail.com>
2023-09-03 20:00:24 +0300

    asdf

--- middlewares/errors/errorHandler.js
+++ middlewares/errors/errorHandler.js
@@ -2,7 +2,7 @@ module.exports = ((error, req, res, next) => {
   const { status = 500, message } = error;
 
   res.status(status).send({
-    message: status === 500 ? 'Ошибка сервера' : message,
+    message: error,
   });
   next();
 });
